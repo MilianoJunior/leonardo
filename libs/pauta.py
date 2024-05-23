97c9738dc0252008908e9ee668bac002e0d21f91
MilianoJunior <jrmfilho37@gmail.com>
2024-05-23 07:36:49 -0300

    config timeline A2

---
+++ libs/pauta.py
@@ -0,0 +1,25 @@
+'''
+
+Pauta de reivindicações
+
+Argumentos - Entendem-se por argumentos as razões que justificam a reivindicação. Eles devem ser claros, objetivos e fundamentados em fatos e dados. A argumentação deve ser feita de forma lógica e coerente, de modo a convencer o interlocutor da legitimidade da reivindicação.
+
+Constituição dos argumentos
+    Tópico: Título do argumento
+    Fundamentação: Fatos e dados que justificam a reivindicação
+    Forma de exposição: Como o argumento será apresentado para o apelo público
+
+
+    Argumento A: Histórico de reajustes salariais para o servidor público do estado de Santa Catarina
+
+        Fundamentação:
+            1. Em 2021, 5% de reajuste salarial, parcelado em 3 vezes, sendo 2% em maio, 1,5% em setembro e 1,5% em dezembro.
+
+
+
+Argumento B: Inflação acumulada no período de 2021 a 2024
+
+Tópico: Descreve
+Fundamentação:
+Forma de exposição:
+'''
